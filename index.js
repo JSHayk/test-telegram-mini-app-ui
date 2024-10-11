a667f9fedc4cb3836ef3e1cd904cb06dbb71563d
jshayk <Rocket2019>
2024-10-11 20:42:24 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const url = "https://66cf-62-89-17-18.ngrok-free.app/";
+
+function getTelegramData() {
+  const Telegram = window.Telegram;
+
+  if (Telegram) {
+    Telegram.ready();
+    return {
+      initData: Telegram.initData,
+      userData: Telegram.initDataUnsafe,
+    };
+  }
+}
+
+async function getAll() {
+  try {
+    fetch(url, {
+      method: "get",
+      headers: new Headers({
+        "ngrok-skip-browser-warning": "69420",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const telegramData = getTelegramData();
+console.log(telegramData);
